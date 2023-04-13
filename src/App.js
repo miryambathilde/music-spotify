c026980ed4bec26a24c1db8ee8100ca3eef7e5f2
Miryam Bathilde <mbathilde@gmail.com>
2023-04-13 13:38:52 +0200

    semantic ui react installed and working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import './scss/global.scss';
+import { Button } from 'semantic-ui-react';
 
 export default function App () {
   return (
     <div>
       <h1>Bienvenido a my Spotify!</h1>
+      <Button primary>Primary</Button>
+      <Button secondary>Secondary</Button>
     </div>
   );
 }
