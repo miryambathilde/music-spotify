67325d5b62bc85522125ef804c60650e8b434a05
Miryam Bathilde <mbathilde@gmail.com>
2023-04-17 11:44:03 +0200

    auth options box

--- src/components/Auth/AuthOptions/AuthOptions.jsx
+++ src/components/Auth/AuthOptions/AuthOptions.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Button } from 'semantic-ui-react';
+import './AuthOptions.scss';
 
 export function AuthOptions (props) {
 
 	const { openLoginForm, openRegisterForm } = props;
 	return (
-		<div style={ { backgroundColor: 'red' } }>
-			<h1>Auth Options</h1>
-			<Button primary onClick={ openRegisterForm }>Registro</Button>
-			<Button secondary onClick={ openLoginForm }>Login</Button>
+		<div className="auth-options">
+			<h1>Millions of free songs on Musicfy</h1>
+			<Button className="register" onClick={ openRegisterForm }>Sign up for free</Button>
+			<Button className="login" onClick={ openLoginForm }>Sign in</Button>
 		</div>
 	);
 }
