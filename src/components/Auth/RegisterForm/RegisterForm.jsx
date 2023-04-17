2fe67765026e6627102973baeabd27403a37d54a
Miryam Bathilde <mbathilde@gmail.com>
2023-04-17 12:17:17 +0200

    register form

--- src/components/Auth/RegisterForm/RegisterForm.jsx
+++ src/components/Auth/RegisterForm/RegisterForm.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react';
+import { Form, Icon } from 'semantic-ui-react';
+import './RegisterForm.scss';
 
 export function RegisterForm (props) {
 
 	const { openLoginForm, goBack } = props;
 
 	return (
-		<div style={ { backgroundColor: 'red' } }>
-			<h1>Register Form</h1>
-			<Button primary onClick={ openLoginForm }>Login</Button>
-			<Button secondary onClick={ goBack }>Atrás</Button>
+		<div className="register-form">
+			<h1>Create your free account and get access to millions of songs</h1>
+			<Form>
+				<Form.Input
+					type="text"
+					placeholder="Email"
+					icon="mail outline"
+				/>
+				<Form.Input
+					type="password"
+					placeholder="Introduce your password"
+					icon={ <Icon name="eye" link onClick={ () => console.log('Show password') }
+					/>
+					}
+				/>
+				<Form.Input
+					type="text"
+					placeholder="What should we call you?"
+					icon="user circle outline"
+				/>
+				<Form.Button type="submit" primary fluid>Submit</Form.Button>
+			</Form>
+
+			<div className="register-form__options">
+				<p onClick={ goBack }>Back</p>
+				<p>Already have a Musicfy? <span onClick={ openLoginForm }>Sign in</span></p>
+			</div>
 		</div>
 	);
 }
