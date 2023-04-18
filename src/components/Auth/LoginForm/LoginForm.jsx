9790c351c74a32bcf27275cc106f86d3932537fe
Miryam Bathilde <mbathilde@gmail.com>
2023-04-18 11:29:31 +0200

    loginForm & logout

--- src/components/Auth/LoginForm/LoginForm.jsx
+++ src/components/Auth/LoginForm/LoginForm.jsx
@@ -1,14 +1,74 @@
-import React from 'react';
-import { Button } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Form, Icon } from 'semantic-ui-react';
+import { useFormik } from "formik";
+import { Auth } from '../../../api';
+import { initialValues, validationSchema } from './LoginForm.data';
+import "./LoginForm.scss";
+
+const auth = new Auth();
 
 export function LoginForm (props) {
 
 	const { openRegisterForm, goBack } = props;
+
+	const [ showPassword, setShowPassword ] = useState(false);
+
+	const onShowOrHiddenPassword = () => setShowPassword(prevState => !prevState);
+
+
+	const formik = useFormik({
+		initialValues: initialValues(),
+		validationSchema: validationSchema(),
+		validateOnChange: false,
+		onSubmit: async (formValue) => {
+			console.log('Formulario login enviado');
+			console.log(formValue);
+			try {
+				await auth.login(formValue.email, formValue.password);
+			} catch (error) {
+				console.error(error);
+			}
+		}
+	});
+
 	return (
-		<div style={ { backgroundColor: 'red' } }>
-			<h1>Login Form</h1>
-			<Button primary onClick={ openRegisterForm }>Register</Button>
-			<Button secondary onClick={ goBack }>Atras</Button>
+		<div className="login-form">
+			<h1>Music for everyone</h1>
+			<Form onSubmit={ formik.handleSubmit }>
+				<Form.Input
+					name="email"
+					type="text"
+					placeholder="Introduce your email"
+					icon="mail outline"
+					onChange={ formik.handleChange }
+					value={ formik.values.email }
+					error={ formik.errors.email }
+				/>
+
+				<Form.Input
+					name="password"
+					type={ showPassword ? "text" : "password" }
+					placeholder="Introduce your password"
+					icon={ <Icon
+						name={ showPassword ? "eye slash" : "eye" }
+						link onClick={ onShowOrHiddenPassword }
+					/>
+					}
+					onChange={ formik.handleChange }
+					value={ formik.values.password }
+					error={ formik.errors.password }
+				/>
+				<Form.Button
+					type="submit"
+					primary fluid loading={ formik.isSubmitting }
+
+				>Submit</Form.Button>
+				<div className="login-form__options">
+					<p onClick={ goBack }>Go back</p>
+					<p>Don't have an account yet? <span onClick={ openRegisterForm }>Sign up</span></p>
+				</div>
+
+			</Form>
 		</div>
 	);
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react';
+import { Auth } from '../../api';
+
+const auth = new Auth();
 
 export function Home () {
 	return (
 		<div>
 			<h1>Home</h1>
+			<Button color="red" onClick={ auth.logout }>Logout</Button>
 		</div>
 	);
 }
